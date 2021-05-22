3d2a66cde9969c0b23b6e9053f824d6f02838d52
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-05-22 11:18:15 +0100

    project init, set up gulp compiler for scss

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # may-portfolio-new
+
+# tools
+
+ - HTML5 & CSS3 (compiled from SCSS using gulp)
+ - Javascript ES6
+ - Desktop-first workflow
+
+# resources
+
+ - [Google Fonts](https://fonts.google.com/)
+
+# gulp setup
+
+ - run `npm install` in the terminal from the root directory
+ - run `./node_modules/gulp/bin/gulp.js sass` from the root directory to compile scss into css
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+function sassCompile(cb) {
+    return gulp.src("public/scss/style.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("public/css"));
+    cb();
+}
+
+function watch() {
+    sassCompile()
+    gulp.watch("app/scss/**/*.scss", sassCompile);
+}
+
+exports.sass = sassCompile;
+exports.watch = watch;
